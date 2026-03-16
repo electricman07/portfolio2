38d74e073143e0e4212e6dd66ee27c41f204d4cd
Glen Popowich <glenp@telus.net>
2026-03-16 12:05:22 -0700

    adjusted font size and margins onn small screen

--- src/routes/stack.tsx
+++ src/routes/stack.tsx
@@ -8,15 +8,15 @@ export const Route = createFileRoute("/stack")({
 function RouteComponent() {
   return (
     <section
-      className="section-stack sections flex flex-col items-center m-20"
+      className="section-stack sections flex flex-col items-center m-2.5 md:m-20"
       id="stack"
     >
-      <h2 className="stack-title section-title p-2.5 w-auto text-center text-5xl mt-17.5 rounded-xl">
+      <h2 className="stack-title section-title p-2.5 w-auto text-center text-2xl  md:text-5xl mt-17.5 rounded-xl">
         The Web Design Technical Stack
       </h2>
-      <article className="stack-article-container p-8 mt-40">
+      <article className="stack-article-container p-1 md:p-8 mt-10 md:mt-40">
         <section>
-          <h2 className="text-3xl font-bold p-5">
+          <h2 className="text-2xl md:text-3xl font-bold p-5">
             Introduction to Web Design and Development
           </h2>
           <p>
