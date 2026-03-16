565096a4474f383ee4267fb0eb68e89276c501fd
Glen Popowich <glenp@telus.net>
2026-03-16 12:07:46 -0700

    adjusted font size and container width

--- src/routes/faq.tsx
+++ src/routes/faq.tsx
@@ -9,8 +9,8 @@ export const Route = createFileRoute("/faq")({
 function RouteComponent() {
   return (
     <section className="section-4 sections flex flex-col items-center" id="faq">
-      <div className="faq-container w-[60%] p-5 mx-auto my-0  rounded-xl">
-        <h2 className="faq-title section-title text-5xl text-center  rounded-xl mt-17.5">
+      <div className="faq-container w-full md:w-[60%] p-5 mx-auto my-0  rounded-xl">
+        <h2 className="faq-title section-title text-2xl md:text-5xl text-center  rounded-xl mt-17.5">
           Frequently Asked Questions
         </h2>
 
