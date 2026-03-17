29a97785f5bf0670d0414138191902f58ee6079e
Glen Popowich <glenp@telus.net>
2026-03-17 16:49:23 -0700

    changed images to use site images

--- src/routes/portfolio.tsx
+++ src/routes/portfolio.tsx
@@ -1,23 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import ImageSlider from "#/components/ImageSlider";
-import flowerImg from "../images/flower.jpg";
-import lighthouseImg from "../images/lighthouse.jpg";
-import dandelion from "../images/dandelion.jpg";
-import sunsetImg from "../images/sunset.jpg";
+import Rylee from "../../public/images/websites/rylee.png";
+import Rylee2 from "../../public/images/websites/Rylee2.png";
 
 export const Route = createFileRoute("/portfolio")({
   component: RouteComponent,
 });
 
 const images = [
-  lighthouseImg,
-  flowerImg,
-  dandelion,
-  sunsetImg,
-  lighthouseImg,
-  flowerImg,
-  dandelion,
-  sunsetImg,
+  Rylee,
+  Rylee2,
+  Rylee,
+  Rylee2,
   // Add more image URLs here
 ];
 
@@ -25,13 +19,13 @@ function RouteComponent() {
   return (
     <>
       <section
-        className="section-5 sections  flex flex-col items-center justify-center rounded-xl h-[80vh]"
+        className="section-5 sections  flex flex-col items-center justify-center rounded-xl h-full"
         id="portfolio"
       >
-        <h2 className="portfolio-title section-title text-5xl text-center mt-17.5 p-2.5 w-auto rounded-xl mb-40">
+        <h2 className="portfolio-title section-title text-2xl md:text-5xl text-center mt-17.5 p-2.5 w-auto rounded-xl mb-40">
           Portfolio
         </h2>
-        <div className="flex flex-col pt-32 pl-9 pr-9 bg-[var(--bg-transparent)] shadow-[var(--box-shadow)] w-[90%] h-[80%] rounded-xl">
+        <div className="flex flex-col  pl-9 pr-9  w-[90%] h-[80%] rounded-xl">
           <ImageSlider images={images} />
         </div>
       </section>
